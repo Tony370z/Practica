28c4b54868cd86384f5d58eb354951200e1aa2da
dlcortez <dlcortez@gmail.com>
2018-11-20 14:44:03 -0400

    ACT001: Cambio en los datos human

--- RestService-1/src/main/java/com/example/demo/service/HumanServiceImpl.java
+++ RestService-1/src/main/java/com/example/demo/service/HumanServiceImpl.java
@@ -10,8 +10,8 @@ public class HumanServiceImpl implements HumanService{
 	@Override
 	public Human get() {
 		Human human = new Human();
-		human.setAge("44");
-		human.setName("Jhon");
+		human.setAge("33");
+		human.setName("Douglas");
 		return human;
 	}
 	
